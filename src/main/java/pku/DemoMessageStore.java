6c2a13cf7cbd55076fc9e5ebdf65f3da8af58a39
hanzhenrui <1801210525@pku.edu.cn>
2018-12-03 23:12:10 +0800

    version2.0

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -368,6 +368,7 @@ public class DemoMessageStore {
 			msg.putHeaders(Skey14,Svalue14);
 			msg.putHeaders(Skey15,Svalue15);
 			msg.putHeaders(MessageHeader.TOPIC,new String(byteTopic));
+			msg.setBody(body);
 			//System.out.println(new String(body));
 			return msg;
 		} catch (IOException e) {
