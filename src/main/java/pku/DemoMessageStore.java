11372153a59625fe0b1925d7aa237baf584ca2ac
hanzhenrui <1801210525@pku.edu.cn>
2018-12-07 21:55:48 +0800

    version3.0

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -5,7 +5,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -14,20 +13,20 @@ import java.util.zip.GZIPOutputStream;
 public class DemoMessageStore {
 	static final DemoMessageStore store = new DemoMessageStore();
 
-    HashMap<String,DataOutputStream> files = new HashMap<>();
+	HashMap<String, DataOutputStream> files = new HashMap<>();
 	static HashMap<String, ArrayList<ByteMessage>> msgs = new HashMap<>();
 
 	//static HashMap<String, BufferedInputStream> bufferInput = new HashMap<>();
 	HashMap<String, DataInputStream> bufferInput = new HashMap<>();
-	static AtomicInteger pushCount = new AtomicInteger();
+	//static AtomicInteger pushCount = new AtomicInteger();
 
 
-	static AtomicInteger count = new AtomicInteger(0);
+	//static AtomicInteger count = new AtomicInteger(0);
 
 	/**************push**************/
 	public void flush() throws IOException {
-		for (String file:files.keySet()){
-			files.get(file).flush();
+		for (String file : files.keySet()) {
+			files.get(file).close();
 		}
 
 	}
@@ -65,23 +64,24 @@ public class DemoMessageStore {
 		//pushCount.incrementAndGet();
 
 	}
-/**************pull******************/
-	 ByteMessage pull(String topic) throws IOException{
-		 byte[] byteHeaderLength;
-		 byte[] headercontent;
-		 byte[] byteBodyLength;
-		 byte[] bodycontent;
-		 String header;
-		 //System.out.println(Thread.currentThread().getName());
+
+	/**************pull******************/
+	ByteMessage pull(String topic) throws IOException {
+		byte[] byteHeaderLength;
+		byte[] headercontent;
+		byte[] byteBodyLength;
+		byte[] bodycontent;
+		String header;
+		//System.out.println(Thread.currentThread().getName());
 
 		String toc = topic + Thread.currentThread().getName();
 		if (!bufferInput.containsKey(toc)) {
-			File file = new File("data/"+topic);
-			if (!file.exists()){
+			File file = new File("data/" + topic);
+			if (!file.exists()) {
 				return null;
 			}
 
-			FileInputStream in = new FileInputStream("data/"+topic);
+			FileInputStream in = new FileInputStream("data/" + topic);
 			BufferedInputStream bufferIn = new BufferedInputStream(in);
 			DataInputStream bufferin = new DataInputStream(bufferIn);
 			bufferInput.put(toc, bufferin);
@@ -90,59 +90,59 @@ public class DemoMessageStore {
 		DataInputStream bufferin = bufferInput.get(toc);
 
 
-		 byteHeaderLength = new byte[4];
-		 int ret = bufferin.read(byteHeaderLength);
-		 if (ret == -1) {
-			 bufferin.close();
-			 return null;
-		 }
-		 int lenofheader = Byte2Int(byteHeaderLength);
-		 headercontent = new byte[lenofheader];
-		 bufferin.read(headercontent);
-		 header = new String(headercontent);//读头部
+		byteHeaderLength = new byte[4];
+		int ret = bufferin.read(byteHeaderLength);
+		if (ret == -1) {
+			bufferin.close();
+			return null;
+		}
+		int lenofheader = Byte2Int(byteHeaderLength);
+		headercontent = new byte[lenofheader];
+		bufferin.read(headercontent);
+		header = new String(headercontent);//读头部
 
-		 byteBodyLength = new byte[4];
-		 bufferin.read(byteBodyLength);//读body
-		 int intBodyLength = Byte2Int(byteBodyLength);
+		byteBodyLength = new byte[4];
+		bufferin.read(byteBodyLength);//读body
+		int intBodyLength = Byte2Int(byteBodyLength);
 
-		 bodycontent = new byte[intBodyLength];
-		 bufferin.read(bodycontent);
+		bodycontent = new byte[intBodyLength];
+		bufferin.read(bodycontent);
 
 
-		 DefaultMessage msg = new DefaultMessage(bodycontent);
-		 DefaultKeyValue keyValue = makeKeyValue(header);
-		 msg.setHeaders(keyValue);//设置头部
-		 return msg;
+		DefaultMessage msg = new DefaultMessage(bodycontent);
+		DefaultKeyValue keyValue = makeKeyValue(header);
+		msg.setHeaders(keyValue);//设置头部
+		return msg;
 	}
 
 	private static DefaultKeyValue makeKeyValue(String header) {
 
 		String[] split = header.split(",");
 
-		    DefaultKeyValue defaultKeyValue = new DefaultKeyValue();
-			defaultKeyValue.put(MessageHeader.MESSAGE_ID, split[0]);
-			defaultKeyValue.put(MessageHeader.TOPIC, split[1]);
-			defaultKeyValue.put(MessageHeader.BORN_TIMESTAMP, split[2]);
-			defaultKeyValue.put(MessageHeader.BORN_HOST, split[3]);
-			defaultKeyValue.put(MessageHeader.STORE_TIMESTAMP, split[4]);
-			defaultKeyValue.put(MessageHeader.STORE_HOST, split[5]);
-			defaultKeyValue.put(MessageHeader.START_TIME, split[6]);
-			defaultKeyValue.put(MessageHeader.STOP_TIME, split[7]);
-			defaultKeyValue.put(MessageHeader.TIMEOUT, split[8]);
-			defaultKeyValue.put(MessageHeader.PRIORITY, split[9]);
-			defaultKeyValue.put(MessageHeader.RELIABILITY, split[10]);
-			defaultKeyValue.put(MessageHeader.SEARCH_KEY, split[11]);
-			defaultKeyValue.put(MessageHeader.SCHEDULE_EXPRESSION, split[12]);
-			defaultKeyValue.put(MessageHeader.SHARDING_KEY, split[13]);
-			defaultKeyValue.put(MessageHeader.SHARDING_PARTITION, split[14]);
-			defaultKeyValue.put(MessageHeader.TRACE_ID, split[15]);
+		DefaultKeyValue defaultKeyValue = new DefaultKeyValue();
+		defaultKeyValue.put(MessageHeader.MESSAGE_ID, split[0]);
+		defaultKeyValue.put(MessageHeader.TOPIC, split[1]);
+		defaultKeyValue.put(MessageHeader.BORN_TIMESTAMP, split[2]);
+		defaultKeyValue.put(MessageHeader.BORN_HOST, split[3]);
+		defaultKeyValue.put(MessageHeader.STORE_TIMESTAMP, split[4]);
+		defaultKeyValue.put(MessageHeader.STORE_HOST, split[5]);
+		defaultKeyValue.put(MessageHeader.START_TIME, split[6]);
+		defaultKeyValue.put(MessageHeader.STOP_TIME, split[7]);
+		defaultKeyValue.put(MessageHeader.TIMEOUT, split[8]);
+		defaultKeyValue.put(MessageHeader.PRIORITY, split[9]);
+		defaultKeyValue.put(MessageHeader.RELIABILITY, split[10]);
+		defaultKeyValue.put(MessageHeader.SEARCH_KEY, split[11]);
+		defaultKeyValue.put(MessageHeader.SCHEDULE_EXPRESSION, split[12]);
+		defaultKeyValue.put(MessageHeader.SHARDING_KEY, split[13]);
+		defaultKeyValue.put(MessageHeader.SHARDING_PARTITION, split[14]);
+		defaultKeyValue.put(MessageHeader.TRACE_ID, split[15]);
 
-			return defaultKeyValue;
+		return defaultKeyValue;
 
 	}
 
 	private static void save() throws Exception {
-	 	//System.out.println(Thread.currentThread().getName());
+		//System.out.println(Thread.currentThread().getName());
 
 		FileOutputStream fos;
 		BufferedOutputStream bos;
@@ -220,20 +220,19 @@ public class DemoMessageStore {
 	}
 
 
-
-	public synchronized static int Byte2Int(byte[]bytes) {
-		return (bytes[0]&0xff)<<24
-				| (bytes[1]&0xff)<<16
-				| (bytes[2]&0xff)<<8
-				| (bytes[3]&0xff);
+	public synchronized static int Byte2Int(byte[] bytes) {
+		return (bytes[0] & 0xff) << 24
+				| (bytes[1] & 0xff) << 16
+				| (bytes[2] & 0xff) << 8
+				| (bytes[3] & 0xff);
 	}
 
-	private synchronized static byte[]intTobyte(int num){
-		byte[]bytes=new byte[4];
-		bytes[0]=(byte) ((num>>24)&0xff);
-		bytes[1]=(byte) ((num>>16)&0xff);
-		bytes[2]=(byte) ((num>>8)&0xff);
-		bytes[3]=(byte) (num&0xff);
+	private synchronized static byte[] intTobyte(int num) {
+		byte[] bytes = new byte[4];
+		bytes[0] = (byte) ((num >> 24) & 0xff);
+		bytes[1] = (byte) ((num >> 16) & 0xff);
+		bytes[2] = (byte) ((num >> 8) & 0xff);
+		bytes[3] = (byte) (num & 0xff);
 		return bytes;
 	}
 
@@ -260,12 +259,5 @@ public class DemoMessageStore {
 		return result.getBytes();
 	}
 
-
-
-	public static void clear() throws Exception {
-		save();
-		msgs.clear();
-		count.set(0);
-	}
 }
 
