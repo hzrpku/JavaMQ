8c413672f6c9c467598d1104af3d07e1219fb76f
hanzhenrui <1801210525@pku.edu.cn>
2018-12-08 18:57:08 +0800

    version3.0

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -3,7 +3,6 @@ package pku;
 import java.io.*;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -22,11 +21,8 @@ public class DemoMessageStore {
 	}
 	/**************push**************/
 	public void push(ByteMessage msg, String topic) throws Exception {
-
-		byte[] byteheader;
-		byte[] lenofheader;
 		byte[] body;
-		//byte[] lenofbody;
+
 
 		DataOutputStream dataout;
 		synchronized (files) {
@@ -67,7 +63,7 @@ public class DemoMessageStore {
 			dataout.writeUTF(msg.headers().getString(MessageHeader.STORE_HOST));
 			dataout.writeUTF(msg.headers().getString(MessageHeader.SEARCH_KEY));
 			dataout.writeUTF(msg.headers().getString(MessageHeader.SCHEDULE_EXPRESSION));
-			dataout.writeUTF(msg.headers().getString(MessageHeader.TRACE_ID));
+			dataout.writeInt(msg.headers().getInt(MessageHeader.TRACE_ID));
 
 			dataout.writeShort(body.length);//写body
 			dataout.write(body);
@@ -118,7 +114,7 @@ public class DemoMessageStore {
 		msg.putHeaders(MessageHeader.STORE_HOST,bufferin.readUTF());
 		msg.putHeaders(MessageHeader.SEARCH_KEY,bufferin.readUTF());
 		msg.putHeaders(MessageHeader.SCHEDULE_EXPRESSION,bufferin.readUTF());
-		msg.putHeaders(MessageHeader.TRACE_ID,bufferin.readUTF());
+		msg.putHeaders(MessageHeader.TRACE_ID,bufferin.readInt());
 
 		short bodylenth = bufferin.readShort();//读body
 		bodycontent = new byte[bodylenth];
@@ -134,73 +130,6 @@ public class DemoMessageStore {
 
 	}
 
-	private static DefaultKeyValue makeKeyValue(String header) {
-
-		String[] split = header.split(",");
-
-		DefaultKeyValue defaultKeyValue = new DefaultKeyValue();
-		defaultKeyValue.put(MessageHeader.MESSAGE_ID, split[0]);
-		defaultKeyValue.put(MessageHeader.TOPIC, split[1]);
-		defaultKeyValue.put(MessageHeader.BORN_TIMESTAMP, split[2]);
-		defaultKeyValue.put(MessageHeader.BORN_HOST, split[3]);
-		defaultKeyValue.put(MessageHeader.STORE_TIMESTAMP, split[4]);
-		defaultKeyValue.put(MessageHeader.STORE_HOST, split[5]);
-		defaultKeyValue.put(MessageHeader.START_TIME, split[6]);
-		defaultKeyValue.put(MessageHeader.STOP_TIME, split[7]);
-		defaultKeyValue.put(MessageHeader.TIMEOUT, split[8]);
-		defaultKeyValue.put(MessageHeader.PRIORITY, split[9]);
-		defaultKeyValue.put(MessageHeader.RELIABILITY, split[10]);
-		defaultKeyValue.put(MessageHeader.SEARCH_KEY, split[11]);
-		defaultKeyValue.put(MessageHeader.SCHEDULE_EXPRESSION, split[12]);
-		defaultKeyValue.put(MessageHeader.SHARDING_KEY, split[13]);
-		defaultKeyValue.put(MessageHeader.SHARDING_PARTITION, split[14]);
-		defaultKeyValue.put(MessageHeader.TRACE_ID, split[15]);
-
-		return defaultKeyValue;
-
-	}
-
-
-	public synchronized static int Byte2Int(byte[] bytes) {
-		return (bytes[0] & 0xff) << 24
-				| (bytes[1] & 0xff) << 16
-				| (bytes[2] & 0xff) << 8
-				| (bytes[3] & 0xff);
-	}
-
-	private synchronized static byte[] intTobyte(int num) {
-		byte[] bytes = new byte[4];
-		bytes[0] = (byte) ((num >> 24) & 0xff);
-		bytes[1] = (byte) ((num >> 16) & 0xff);
-		bytes[2] = (byte) ((num >> 8) & 0xff);
-		bytes[3] = (byte) (num & 0xff);
-		return bytes;
-	}
-
-
-	private static byte[] header(KeyValue headers) {
-
-		Map<String, Object> map = headers.getMap();
-		String result = String.valueOf(map.get(MessageHeader.MESSAGE_ID)) + "," +
-				map.get(MessageHeader.TOPIC) + "," +
-				map.get(MessageHeader.BORN_TIMESTAMP) + "," +
-				map.get(MessageHeader.BORN_HOST) + "," +
-				map.get(MessageHeader.STORE_TIMESTAMP) + "," +
-				map.get(MessageHeader.STORE_HOST) + "," +
-				map.get(MessageHeader.START_TIME) + "," +
-				map.get(MessageHeader.STOP_TIME) + "," +
-				map.get(MessageHeader.TIMEOUT) + "," +
-				map.get(MessageHeader.PRIORITY) + "," +
-				map.get(MessageHeader.RELIABILITY) + "," +
-				map.get(MessageHeader.SEARCH_KEY) + "," +
-				map.get(MessageHeader.SCHEDULE_EXPRESSION) + "," +
-				map.get(MessageHeader.SHARDING_KEY) + "," +
-				map.get(MessageHeader.SHARDING_PARTITION) + "," +
-				map.get(MessageHeader.TRACE_ID);
-		return result.getBytes();
-	}
-
-	//压缩
 	public static byte[] msg2byte_gzip(byte[] data) {
 		byte[] b = null;
 		try {
@@ -216,7 +145,7 @@ public class DemoMessageStore {
 		}
 		return b;
 	}
-	//解压缩
+
 	public static byte[] byte2msg_gzip(byte[] data) {
 		byte[] b = null;
 		try {
@@ -240,4 +169,3 @@ public class DemoMessageStore {
 	}
 
 }
-
