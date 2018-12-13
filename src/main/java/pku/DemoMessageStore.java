6a3678d6b95820ef29f541e61d976c17575af2e1
hanzhenrui <1801210525@pku.edu.cn>
2018-12-13 09:20:07 +0800

    version3.0

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -38,7 +38,7 @@ public class DemoMessageStore {
 		}
 		byte bodytype;
 
-		if (msg.getBody().length>150){
+		if (msg.getBody().length>15000){
 			body = msg2byte_gzip(msg.getBody());
 			bodytype=1;
 		}
