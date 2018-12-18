4a747787a459f918471d2d6f5f74ab0acd295b15
hanzhenrui <1801210525@pku.edu.cn>
2018-12-19 01:12:42 +0800

    version3.0

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -6,9 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
-//import java.util.zip.ZipOutputStream;
-//import java.util.zip.ZipInputStream;
-//import java.util.zip.ZipEntry;
+
 
 
 public class DemoMessageStore {
@@ -40,7 +38,7 @@ public class DemoMessageStore {
 		}
 		byte bodytype;
 
-		if (msg.getBody().length>100){
+		if (msg.getBody().length>256){
 			body =msg2byte_gzip(msg.getBody());
 			bodytype=1;
 		}
@@ -180,49 +178,4 @@ public class DemoMessageStore {
 		}
 		return b;
 	}
-/*
-	public static byte[] zip(byte[] data) {
-		byte[] b = null;
-		try {
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ZipOutputStream zip = new ZipOutputStream(bos);
-			ZipEntry entry = new ZipEntry("zip");
-			entry.setSize(data.length);
-			zip.putNextEntry(entry);
-			zip.write(data);
-			zip.closeEntry();
-			zip.close();
-			b = bos.toByteArray();
-			bos.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return b;
-	}
-
-	public static byte[] unZip(byte[] data) {
-		byte[] b = null;
-		try {
-			ByteArrayInputStream bis = new ByteArrayInputStream(data);
-			ZipInputStream zip = new ZipInputStream(bis);
-			while (zip.getNextEntry() != null) {
-				byte[] buf = new byte[1024];
-				int num = -1;
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				while ((num = zip.read(buf, 0, buf.length)) != -1) {
-					baos.write(buf, 0, num);
-				}
-				b = baos.toByteArray();
-				baos.flush();
-				baos.close();
-			}
-			zip.close();
-			bis.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return b;
-	}
-	*/
-
 }
